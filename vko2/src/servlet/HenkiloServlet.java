a172b6dd71091b223b61384f7825cae8d8a914a9
Robert Ridal <robert2rid@yahoo.com>
2017-09-04 20:05:44 +0300

    Add files via upload

---
+++ vko2/src/servlet/HenkiloServlet.java
@@ -0,0 +1,75 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import henkilo.Henkilo;
+import poikkeus.DAOPoikkeus;
+import poikkeus.HenkiloDAO;
+
+/**
+ * Servlet implementation class HenkiloServlet
+ */
+@WebServlet("/henkilot")
+public class HenkiloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HenkiloServlet() {
+        super();
+    }
+
+    
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String enimi = request.getParameter("etunimi");
+		String snimi = request.getParameter("sukunimi");
+		
+		Henkilo h = new Henkilo(enimi, snimi);
+
+		try {
+			HenkiloDAO hDao = new HenkiloDAO();
+			hDao.lisaa(h);
+		} catch (DAOPoikkeus e) {
+			throw new ServletException(e);
+		}
+		
+		
+		response.sendRedirect("henkilot"); //redirect doGet
+	}
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Henkilo> henkilot;
+		
+		try {
+			//tietokannasta henkilöt
+			HenkiloDAO hDao = new HenkiloDAO();
+			henkilot = hDao.haeKaikki();
+		} catch(DAOPoikkeus e) {
+			throw new ServletException(e);
+		}
+		
+		//requestiin talteen
+		request.setAttribute("henkilot", henkilot);
+		
+		//jsp hoitaa muotoilun
+		request.getRequestDispatcher("henkilot.jsp").forward(request, response);
+		
+	}
+
+}
